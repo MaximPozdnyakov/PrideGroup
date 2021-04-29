1510327f5e4b69bdea40e44052669f2e1dd91d1d
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-04-29 18:47:32 +0300

    feat: add slider to portfolio section

--- src/main.js
+++ src/main.js
@@ -1,17 +1,31 @@
-let sliderCount = 1;
-document
-  .querySelector("#portfolio-arrow-left")
-  .addEventListener("click", () => {
-    sliderCount = sliderCount - 1 == 0 ? 12 : sliderCount - 1;
-    document.querySelector("#portfolio-slider-counter").textContent = String(
-      sliderCount
-    );
-  });
-document
-  .querySelector("#portfolio-arrow-right")
-  .addEventListener("click", () => {
-    sliderCount = sliderCount + 1 == 13 ? 1 : sliderCount + 1;
-    document.querySelector("#portfolio-slider-counter").textContent = String(
-      sliderCount
-    );
-  });
+window.addEventListener("load", function () {
+  let sliderCount = 1;
+  document
+    .querySelector("#portfolio-arrow-left")
+    .addEventListener("click", () => {
+      document
+        .querySelector(`#slide-${sliderCount}`)
+        .classList.toggle("hidden");
+      sliderCount = sliderCount - 1 == 0 ? 12 : sliderCount - 1;
+      document.querySelector("#portfolio-slider-counter").textContent = String(
+        sliderCount
+      );
+      document
+        .querySelector(`#slide-${sliderCount}`)
+        .classList.toggle("hidden");
+    });
+  document
+    .querySelector("#portfolio-arrow-right")
+    .addEventListener("click", () => {
+      document
+        .querySelector(`#slide-${sliderCount}`)
+        .classList.toggle("hidden");
+      sliderCount = sliderCount + 1 == 13 ? 1 : sliderCount + 1;
+      document.querySelector("#portfolio-slider-counter").textContent = String(
+        sliderCount
+      );
+      document
+        .querySelector(`#slide-${sliderCount}`)
+        .classList.toggle("hidden");
+    });
+});
